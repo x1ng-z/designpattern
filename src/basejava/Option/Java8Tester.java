962f690f09371d3400d71497c11b68bf06f4099b
x1ng-z <37235318+x1ng-z@users.noreply.github.com>
2021-04-07 09:54:20 +0800

    zzx II

--- src/basejava/Option/Java8Tester.java
+++ src/basejava/Option/Java8Tester.java
@@ -14,6 +14,7 @@ public class Java8Tester {
         //hs compute change test
         //zzx compuet change i
         //hs compute change II
+        //zzx II
         Java8Tester java8Tester = new Java8Tester();
         Integer value1 = null;
         Integer value2 = new Integer(10);
